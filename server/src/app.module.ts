f0b1225fcf878a58f2e193db848e6ed0e55a4fe2
zhumabek <jumabekzalkarov@gmail.com>
2020-08-23 00:10:01 +0600

    deleted unnecessary controllers and services

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import {typeOrmConfig} from "./config/database/typeorm.config";
@@ -9,8 +7,6 @@ import {typeOrmConfig} from "./config/database/typeorm.config";
   imports: [
       TypeOrmModule.forRoot(typeOrmConfig),
       ConfigModule.forRoot()
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  ]
 })
 export class AppModule {}
