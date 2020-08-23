b855221ede16b230e5eeafb944ec31e5d969301b
zhumabek <jumabekzalkarov@gmail.com>
2020-08-23 19:52:32 +0600

    injecting new modules to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import {typeOrmConfig} from "./config/database/typeorm.config";
+import {CategoryModule} from "./modules/category/category.module";
+import {ProductModule} from "./modules/product/product.module";
 
 @Module({
   imports: [
       TypeOrmModule.forRoot(typeOrmConfig),
-      ConfigModule.forRoot()
+      ConfigModule.forRoot(),
+      CategoryModule,
+      ProductModule
   ]
 })
 export class AppModule {}
