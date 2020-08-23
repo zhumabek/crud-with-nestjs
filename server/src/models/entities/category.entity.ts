ca112e7f0278e3c0391270eaab883badf1a2ded0
zhumabek <jumabekzalkarov@gmail.com>
2020-08-23 19:50:00 +0600

    new entity category

---
+++ server/src/models/entities/category.entity.ts
@@ -0,0 +1,15 @@
+import {Entity, BaseEntity, Column, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {Product} from "./product.entity";
+
+@Entity("categories")
+export class Category extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column("varchar")
+    title: string;
+
+
+    @OneToMany(type => Product, product => product.category)
+    products: Product[];
+}
