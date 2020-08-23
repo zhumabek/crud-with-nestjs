57fedb182a9a7a17f1f070a49ce12035315da817
zhumabek <jumabekzalkarov@gmail.com>
2020-08-23 21:58:34 +0600

    added crud functionality to Category module

--- server/src/modules/category/category.controller.ts
+++ server/src/modules/category/category.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UsePipes, ValidationPipe} from '@nestjs/common';
 import {CategoryService} from "./category.service";
 import {CreateCategoryDto} from "./dto/create-category.dto";
 import {Category} from "../../models/entities/category.entity";
@@ -8,7 +8,33 @@ export class CategoryController {
     constructor(private readonly categoryService: CategoryService) {}
 
     @Post()
+    @UsePipes(ValidationPipe)
     createCategory(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
         return this.categoryService.createCategory(createCategoryDto)
     }
+
+    @Get(":id")
+    getCategoryById(@Param('id', ParseIntPipe) id: number): Promise<Category> {
+        return this.categoryService.getCategoryById(id)
+    }
+
+    @Get()
+    getCategories(): Promise<Category[]> {
+        return this.categoryService.getCategories()
+    }
+
+    @Put(":id")
+    @UsePipes(ValidationPipe)
+    updateCategory(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() createCategoryDto: CreateCategoryDto
+    ): Promise<Category> {
+        return this.categoryService.updateCategory(id, createCategoryDto)
+    }
+
+    @Delete(":id")
+    deleteCategory(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        return this.categoryService.deleteCategory(id)
+    }
+
 }
--- server/src/modules/category/category.service.ts
+++ server/src/modules/category/category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import {ForbiddenException, Injectable, NotFoundException} from '@nestjs/common';
 import {CreateCategoryDto} from "./dto/create-category.dto";
 import {Category} from "../../models/entities/category.entity";
 import {CategoryRepository} from "../../models/repositories/category.repository";
@@ -11,7 +11,46 @@ export class CategoryService {
         private categoryRepository: CategoryRepository
     ) {}
 
-    createCategory(createCategoryDto: CreateCategoryDto): Promise<Category> {
-        return this.categoryRepository.createCategory(createCategoryDto)
+    async createCategory(createCategoryDto: CreateCategoryDto): Promise<Category> {
+        const { title } = createCategoryDto;
+        const existingCategory = await this.categoryRepository.findOne({title});
+
+        if (existingCategory){
+            throw new ForbiddenException("Category with this title already exist!")
+        }
+
+        const category =  new Category();
+        category.title = title;
+        await category.save();
+        return category;
+    }
+
+    async getCategoryById(id: number): Promise<Category> {
+        const category =  await this.categoryRepository.findOne(id)
+        if(!category){
+            throw new NotFoundException("Category with this id not found!");
+        }
+
+        return category;
+    }
+
+    async getCategories(): Promise<Category[]> {
+        return await this.categoryRepository.find();
+    }
+
+    async updateCategory(id: number, createCategoryDto: CreateCategoryDto): Promise<Category> {
+        const { title } = createCategoryDto;
+        const category = await this.getCategoryById(id);
+
+        category.title = title;
+        await category.save();
+        return category;
+    }
+
+    async deleteCategory(id: number): Promise<void> {
+        const category =  await this.categoryRepository.delete(id);
+        if(!category){
+            throw new NotFoundException("Category was not deleted!");
+        }
     }
 }
