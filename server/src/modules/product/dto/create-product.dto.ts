42ea17fc0b9c230a6e53c22c170db4269a7953bb
zhumabek <jumabekzalkarov@gmail.com>
2020-08-24 00:21:29 +0600

    added validation to create-product types class

--- server/src/modules/product/dto/create-product.dto.ts
+++ server/src/modules/product/dto/create-product.dto.ts
@@ -1,7 +1,21 @@
+import {IsNotEmpty, IsNumber, IsString} from "class-validator";
+
 export class CreateProductDto {
+    @IsString()
+    @IsNotEmpty()
     title: string;
+
+    @IsString()
+    @IsNotEmpty()
     description: string;
+
+    @IsString()
+    @IsNotEmpty()
     image: string;
+
+    @IsNumber()
     price: number;
+
+    @IsNumber()
     category: number
 }
